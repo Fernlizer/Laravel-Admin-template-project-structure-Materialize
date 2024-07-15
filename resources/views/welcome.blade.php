2c96c72a96fe6dac0b70c8b1660ba82bbac9617f
FERN <Fernlizer@hotmail.com>
2024-07-15 11:21:39 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+@section('header')
+@endsection
+@section('content')
+    <div class="row">
+        <div class="col-md-12">
+            <h1 class="text-center">Welcome to Materialize</h1>
+        </div>
+    </div>
+@endsection
+@section('scripts')
+@endsection
